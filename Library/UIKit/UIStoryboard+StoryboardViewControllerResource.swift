cb7a253790cd044f2649d347bf10d3215a194cf5
Tom Lokhorst <tom@lokhorst.eu>
2019-11-27 22:16:40 +0100

    Add instantiateViewController with creator parameter

--- Library/UIKit/UIStoryboard+StoryboardViewControllerResource.swift
+++ Library/UIKit/UIStoryboard+StoryboardViewControllerResource.swift
@@ -21,4 +21,23 @@ public extension UIStoryboard {
   func instantiateViewController<ViewControllerResource: StoryboardViewControllerResourceType>(withResource resource: ViewControllerResource) -> ViewControllerResource.ViewControllerType?  {
     return self.instantiateViewController(withIdentifier: resource.identifier) as? ViewControllerResource.ViewControllerType
   }
+
+  /**
+   Instantiates and returns the view controller with the specified resource (R.storyboard.*.*),  using your custom initialization code.
+
+   - parameter resource: An resource (R.storyboard.*.*) that uniquely identifies the view controller in the storyboard file. If the specified resource does not exist in the storyboard file, this method raises an exception.
+
+   - parameter creator: A custom block to create your view controller. In this block, you must call `init(coder:)` using the `NSCoder` provided as an argument. You can use this block to setup any mandatory fields in your view controller
+
+   - returns: The view controller corresponding to the specified resource (R.storyboard.*.*). If no view controller is associated, this method throws an exception.
+   */
+  @available(iOS 13.0, *)
+  func instantiateViewController<ViewControllerResource: StoryboardViewControllerResourceType>(
+    withResource resource: ViewControllerResource,
+    creator: @escaping (NSCoder) -> ViewControllerResource.ViewControllerType?
+  ) -> ViewControllerResource.ViewControllerType?
+    where ViewControllerResource.ViewControllerType : UIViewController
+  {
+    return self.instantiateViewController(identifier: resource.identifier, creator: creator)
+  }
 }
